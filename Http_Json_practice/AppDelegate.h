a06906a5f7a6e6f97a06a6c402ac4c788a7ba761
HongKyuCho <whghdrb2@hanmail.net>
2017-02-02 16:10:33 +0900

    Initial Commit

---
+++ Http_Json_practice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Http_Json_practice
+//
+//  Created by 조홍규 on 2017. 2. 2..
+//  Copyright © 2017년 조홍규. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Http_Json_practice/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Http_Json_practice
+//
+//  Created by 조홍규 on 2017. 2. 2..
+//  Copyright © 2017년 조홍규. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
