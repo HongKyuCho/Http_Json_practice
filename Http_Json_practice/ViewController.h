71fa4f0107fb4b019fb4fdbb94667011a1aa71b5
HongKyuCho <whghdrb2@hanmail.net>
2017-02-02 18:30:52 +0900

    firstCommit

--- Http_Json_practice/ViewController.h
+++ Http_Json_practice/ViewController.h
@@ -8,8 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UITableViewDelegate, UITableViewDataSource>
 
+@property (weak, nonatomic) IBOutlet UITableView *mainTableView;
+@property (weak, nonatomic) NSArray *jsonArray;
+@property (strong, nonatomic) NSMutableData *data;
 
 @end
-
+    
--- Http_Json_practice/ViewController.m
+++ Http_Json_practice/ViewController.m
@@ -17,6 +17,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [UIApplication sharedApplication].networkActivityIndicatorVisible =YES;
+    NSURL *url = [NSURL URLWithString:@"http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=18d789995cb2a9451b1627f5774bb9e3&targetDt=20170101"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [[NSURLConnection alloc] initWithRequest:request delegate:self];
+
+    
 }
 
 
@@ -24,6 +31,59 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [_jsonArray count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell =[tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Cell"];
+    }
+    
+    cell.textLabel.text = [[_jsonArray objectAtIndex:indexPath.row]objectForKey:@"movieNm"];
+    
+    return  cell;
+    
+    
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response{
+    _data = [[NSMutableData alloc]init];
+    
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data{
+    [_data appendData:data];
+    
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection{
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+    _jsonArray = [NSJSONSerialization JSONObjectWithData:_data options:nil error:nil];
+    [_mainTableView reloadData];
+    
+    
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error{
+    UIAlertController *myAlertController = [UIAlertController alertControllerWithTitle:@"Error" message:@"Please check the connection" preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction *ok = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action){
+        [myAlertController dismissViewControllerAnimated:YES completion:nil];
+    }];
+    [myAlertController addAction:ok];
+    [self presentViewController:myAlertController animated:YES completion:nil];
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+    
+}
+
 
 
 @end
